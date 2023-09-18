934e554073e0afadc160ea9045f7afbabea2bf7d
Inma <iperezs24m@gmail.com>
2023-09-18 08:47:03 +0200

    nuevo

--- src/CohesionFuncional02/mal/UsoMal.java
+++ src/CohesionFuncional02/mal/UsoMal.java
@@ -1,12 +1,25 @@
 package CohesionFuncional02.mal;
 
 public class UsoMal {
+	//ahora usoBien
 public static void main(String[] args) {
 	FooBarMal oBarMal=new FooBarMal();
+	
+	
+	someMethos(oBarMal);
+	
+	
+	otherMethos(oBarMal);
+}
+
+private static void otherMethos(FooBarMal oBarMal) {
+	oBarMal.oneMethod();
+	oBarMal.twoMethod();
+}
+
+private static void someMethos(FooBarMal oBarMal) {
 	oBarMal.FirstMethod();
 	oBarMal.SecondMethod();
 	oBarMal.ThirdMethod();
-	oBarMal.oneMethod();
-	oBarMal.twoMethod();
 }
 }
